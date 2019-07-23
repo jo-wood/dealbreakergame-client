37e00a33b3223b403ce15e38cc99b9f297215358
Junaid <jahmad095@gmail.com>
2019-07-23 10:03:43 -0400

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+
+  return (
+    <footer>
+        <p>Made with Love in Toronto, Canada.</p>
+        <a href="">Privacy</a>
+    </footer>
+  );
+}
+
+export default Footer;
