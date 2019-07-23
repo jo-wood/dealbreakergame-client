cafdb69670e7ecaf948f53556513cdb1cd37330a
Joanna Wood <caitlinjoannawood@gmail.com>
2019-07-23 14:16:11 -0400

    css added to questions on Game comp

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,17 +4,15 @@ class Question extends Component {
   render() {
     const { q } = this.props;
     return (
-      <div>
-        <h4>{ q.question }</h4>
-        <ul>
-          <li>{ q.optionA }</li>
-          <li>{ q.optionB }</li>
-          <li>{ q.optionC }</li>
-          <li>{ q.optionD }</li>
-
-        </ul>
-
-      </div>
+      <div class="hiddenCB">
+        <div>
+          <h1>{q.question}</h1>
+          <input name={q.id} id="cb1" type="checkbox"/><label for="cb1">{q.optionA} </label>
+          <input name={q.id} id="cb2" type="checkbox"/><label for="cb2">{q.optionB}</label>
+          <input name={q.id} id="cb3" type="checkbox"/><label for="cb3">{q.optionC}</label>
+          <input name={q.id} id="cb4" type="checkbox"/><label for="cb4">{q.optionD}</label>
+        </div>   
+      </div>   
     );
   }
 
