ee83fe58a86d21daa48beca01effca85d21c596a
Joanna Wood <caitlinjoannawood@gmail.com>
2019-07-24 14:55:12 -0400

    /profile route added matching dummy_cookie_id to user_id: WIP make user/profile have associated accepted user_ids from match history

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './components/Home';
 import Game from './components/Game'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Privacy from './components/Privacy';
+import Profile from './components/Profile';
 import Loading from './components/Loading';
 import Footer from './components/Footer';
 
@@ -24,10 +25,11 @@ function App() {
           <Nav />
           <Switch>
             <Route path="/" exact component={Home} />
-            <Route path="/login" component={Home} />
-            <Route path="/loading" component={Loading}/>
-            <Route path="/game" component={Game} />
-            <Route path="/privacy" component={Privacy}/>
+            <Route path="/login" exact component={Home} />
+            <Route path="/loading" exact component={Loading}/>
+            <Route path="/game" exact component={Game} />
+            <Route path="/privacy" exact component={Privacy}/>
+            <Route path="/profile" exact component={Profile}/>
           </Switch>
           <Footer/>
         </div>
