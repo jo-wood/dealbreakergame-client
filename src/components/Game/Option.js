4f761c1c87a17b268959f3769ba28e1381b39d98
Joanna Wood <caitlinjoannawood@gmail.com>
2019-07-27 17:14:31 -0400

    preped game, question and option comp for sending to BE

---
+++ src/components/Game/Option.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Option extends Component {
+  _handleClick() {
+    const { option, q_id } = this.refs;
+    const _submitAnswer = this.props.submit;
+    _submitAnswer({ q_id, option });
+  }
+  render() {
+    const { key, option } = this.props;
+      return (
+        <div>
+          <input 
+            key={key}
+            ref={{option, q_id: key }}
+            onClick={this._handleClick} 
+            type="checkbox"/>
+          <label for={option}>{option}</label>
+        </div>
+      );
+  }
+}
+export default Option;
