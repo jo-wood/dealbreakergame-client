28ae3c9beb35c9ba74a5a06e3de4fbd9fe5956dd
Junaid <jahmad095@gmail.com>
2019-07-27 22:39:06 -0400

    count down now works with one line of code

--- src/components/Waiting/Waiting.js
+++ src/components/Waiting/Waiting.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactCountdown from 'react-countdown-now'; //react count down now
 import ChatRoom from './ChatRoom';
 import {Helmet} from "react-helmet";
+import io from 'socket.io-client';
 
 require('dotenv').config({ path: '../../' })
 
@@ -9,10 +10,23 @@ class Waiting extends Component {
   constructor() {
     super();
     this.state = {
-      nextGameTime: new Date(2019, 6, 28, 20, 0, 0)
+      nextGameTime: null
     }
   }
-  
+
+  async componentDidMount() {
+    this.socket = io('http://localhost:5001');
+    const socket = this.socket;
+    socket.on('setNextGameTime', (nextGameTime) => {
+      this._setDateOfNextGame(nextGameTime);
+    });
+  }
+
+  _setDateOfNextGame = (nextGameTime) => {
+    console.log(nextGameTime);
+    this.setState({ nextGameTime: nextGameTime });
+  }
+
   render() {
     return (
       <div>
