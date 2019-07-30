d15797fd23999bfc5950f9ad7da7414ac0908034
Junaid <jahmad095@gmail.com>
2019-07-29 22:52:22 -0400

    add range to blur function

--- src/UtilComponents/SingleContestent.js
+++ src/UtilComponents/SingleContestent.js
@@ -3,31 +3,31 @@ import React, { Component } from 'react';
 class SingleContestent extends Component {
 
   renderPic({ key, img, match } ) {
-    switch (match) {
-      case 100: 
-        return (
-            <div className="crop noblur">
-              <img src={img} key={key} alt="user_img" />
-            </div> 
-        )     
-      case 50:
-        return (
-            <div className="crop halfblur">
-              <img src={img} key={key} alt="user_img" />
-            </div> 
-        )  
-      case 33:
-        return (
-            <div className="crop thirdblur">
-              <img src={img} key={key} alt="user_img" />
-            </div> 
-        )          
-      default:
+    switch (true) {
+      case (match < 33):
         return (
             <div className="crop fullblur">
               <img src={img} key={key} alt="user_img" />
-            </div> 
-        )          
+            </div>
+        )
+      case (match < 50):
+        return (
+            <div className="crop thirdblur">
+              <img src={img} key={key} alt="user_img" />
+            </div>
+        )
+      case (match < 75):
+        return (
+            <div className="crop halfblur">
+              <img src={img} key={key} alt="user_img" />
+            </div>
+        )
+      default:
+        return (
+            <div className="crop noblur">
+              <img src={img} key={key} alt="user_img" />
+            </div>
+        )
     }
   }
 
