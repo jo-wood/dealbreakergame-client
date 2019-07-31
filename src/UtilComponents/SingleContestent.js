fb6a3a3b628e62617a01867a15c2f48037621321
Junaid <jahmad095@gmail.com>
2019-07-31 18:32:23 -0400

    added more incriment for blur

--- src/UtilComponents/SingleContestent.js
+++ src/UtilComponents/SingleContestent.js
@@ -10,15 +10,39 @@ class SingleContestent extends Component {
               <img src={img} key={key} alt="user_img" />
             </div>
         )
+      case (match < 40):
+        return (
+            <div className="crop thirtyblur">
+              <img src={img} key={key} alt="user_img" />
+            </div>
+        )
       case (match < 50):
         return (
-            <div className="crop thirdblur">
+            <div className="crop fortyblur">
+              <img src={img} key={key} alt="user_img" />
+            </div>
+        )
+      case (match < 60):
+          return (
+              <div className="crop fiftyblur">
+                <img src={img} key={key} alt="user_img" />
+              </div>
+          )
+      case (match < 70):
+        return (
+            <div className="crop sixtyblur">
               <img src={img} key={key} alt="user_img" />
             </div>
         )
       case (match < 75):
         return (
-            <div className="crop halfblur">
+            <div className="crop sevenityblur">
+              <img src={img} key={key} alt="user_img" />
+            </div>
+        )
+      case (match < 80):
+        return (
+            <div className="crop eightyblur">
               <img src={img} key={key} alt="user_img" />
             </div>
         )
--- src/components/Game/Results.js
+++ src/components/Game/Results.js
@@ -14,6 +14,7 @@ class Results extends Component {
   }
 
   calculatingMatches() {
+    
     return (
       <div>
         <div className="lds-heart"><div></div></div> 
