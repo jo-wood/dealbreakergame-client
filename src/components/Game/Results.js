59bbcfcde5a11b465a9d629f3a19a15c08e327be
Junaid <jahmad095@gmail.com>
2019-07-31 21:08:05 -0400

    fix to show results game

--- src/components/Game/Results.js
+++ src/components/Game/Results.js
@@ -2,94 +2,81 @@ import React, { Component } from 'react';
 import Result from './SingleResult';
 import {Helmet} from "react-helmet";
 import io from 'socket.io-client';
-import dummyRankedMatches from './socketMessages/finalRankMatches'
-import dummyUserInfo from './socketMessages/usersProfiles';
+// import dummyRankedMatches from './socketMessages/finalRankMatches'
+// import dummyUserInfo from './socketMessages/usersProfiles';
 
-class Results extends Component {
+class Host extends Component {
   constructor() {
     super();
-    this.state = {
-      calculating: true
+    this.state= {
+      user_id: null,
+      rankedMatches: null,
+      grabMatchesInfo: null
     }
   }
 
-  calculatingMatches() {
-    
-    return (
-      <div>
-        <div className="lds-heart"><div></div></div> 
-        <h2> Calculating your top matches!.... </h2>
-      </div>
-    ); 
-  }
-  _handleMatchResults() {
-    let { user_id } = this.props;
-    let currentUser = user_id;
-    let rankedMatches = dummyRankedMatches;
-    let grabMatchesInfo = dummyUserInfo;
-    this.socket = io(process.env.REACT_APP_SOCKET_SERVER_URL || 'http://localhost:5001');
-    this.socket.on('userMatches', (userMatches) => {
-          let ranks = Object.keys(userMatches.rankedMatches);
-          if (ranks.length) {
-            rankedMatches = userMatches.rankedMatches;
-            grabMatchesInfo = userMatches.grabMatchesInfo;
-          }
-      });
-    if (!user_id) {
-      let currentUserString = localStorage.getItem('currentUser');
-      let currentUser = JSON.parse(currentUserString);
-        if (currentUser) {
-          let id = currentUser.user_id;
-          if (!id) {
-            id = 1;
-          }
-          currentUser = id;
-        } else {
-            return (
-              <div>
-                <h1>Please login</h1>
-              </div>
-            );
-        }
-    }
-    if (rankedMatches[user_id]) {
-      const matchesArray = rankedMatches[currentUser].slice(0, 3);
-      this.matchResults({
+  _handleMatchResults = ({ rankedMatches, grabMatchesInfo } ) => {
+    console.log("rankedMatches:",rankedMatches,", grabMatchesInfo: ",grabMatchesInfo);
+      const currentUser = this.state.user_id;
+      const matchesArray = rankedMatches[currentUser].slice(0, 3);      
+      this.setState({ 
         rankedMatches: matchesArray,
-        grabMatchesInfo
+        grabMatchesInfo: grabMatchesInfo
       })
-    } 
   } 
-  matchResults({rankedMatches, grabMatchesInfo }) {
-    return rankedMatches.map(match => {
-      const match_user_id = Object.keys(match);
-      const matchPercent = match[match_user_id];
-      const matchInfo = grabMatchesInfo[match_user_id];
-      const name = matchInfo.full_name;
-      const insta = matchInfo.insta_id;
-      const image = matchInfo.img;
-      this.setState({calculating:false})
+  matchResults() {
+    const { rankedMatches, grabMatchesInfo } = this.state;
+    console.log("STATE: ",rankedMatches, grabMatchesInfo)
+    if (!rankedMatches) {
       return (
-        <div className="oneResult">
-          <Result key={match_user_id} matchData={{ image, name, insta, matchPercent }}/>
+        <div className="Main">
+          <div class="lds-heart"><div></div></div> 
+          <h2> Calculating your top matches!.... </h2>
         </div>
-      );
-    })
-  } 
+      )
+    } else {
+      return rankedMatches.map(match => {
+        console.log("MAPPED MATCH: ", match)
+        const match_user_id = Object.keys(match);
+        const matchPercent = match[match_user_id];
+        const matchInfo = grabMatchesInfo[match_user_id];
+        const name = matchInfo.full_name;
+        const insta = matchInfo.insta_id;
+        const image = matchInfo.img;
+        return (
+          <div className="oneResult">
+            <Result key={match_user_id} matchData={{ image, name, insta, matchPercent }}/>
+          </div>
+        )
+      })
+    }
+  }
+  componentDidMount() {
+    const currentUserString = localStorage.getItem('currentUser');
+    const currentUser = JSON.parse(currentUserString);
+    const user_id = currentUser.user_id || 2 ; 
+    this.setState({ user_id })   
+    this.socket = io('http://localhost:5001');
+    this.socket.on('userMatches', (userMatches) => {
+      console.log("Server sent final game Data: ", userMatches);
+      this._handleMatchResults(userMatches);
+    });
+  }
   render() {
-    const results = (this.state.calculating) ? (<div>{ this.calculatingMatches() }</div>) : (<div> {this._handleMatchResults()}</div>)
+    const results = (<div>{this.matchResults()}</div>);
     return (
       <div>
-      <Helmet>
-      <meta charSet="utf-8" />
-      <title>Results</title>
-      <meta name="description" content="Today's Game results for signed in users. See who you have matched with" />
-      </Helmet>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Results</title>
+            <meta name="description" content="Today's Game results for signed in users. See who you have matched with" />
+          </Helmet>
       <div className="Main">
         {results}
       </div>
-    </div>
+      </div>
     );
   }
 }
-export default Results;
+
+export default Host;
